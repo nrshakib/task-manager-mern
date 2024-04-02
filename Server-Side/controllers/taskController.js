fac5bf0e30a65f956c8c435ffd30d7dbda64e626
NRS\nrsha <nrshakib007@gmail.com>
2024-04-02 15:54:42 +0600

    Added Task Controller

---
+++ Server-Side/controllers/taskController.js
@@ -0,0 +1,62 @@
+const Task = require("../models/TaskSchema");
+
+// Get all tasks
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({ userId: req.user._id });
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Get a single task
+exports.getTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Create a task
+exports.createTask = async (req, res) => {
+  const task = new Task({
+    title: req.body.title,
+    description: req.body.description,
+    userId: req.user._id,
+  });
+
+  try {
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Update a task
+exports.updateTask = async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Delete a task
+exports.deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: "Task deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
